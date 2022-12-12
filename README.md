31ca36cf4090a7fb90dd6c7eeee7380f09853963
nadeemchoudhari <nadim.choudhari@gmail.com>
2022-12-12 15:11:24 +0530

    script updated

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ added --kubelet-insecure-tls in args section of container.
 
 ```
 
-2.  RBAC serviceaccount  is configures to authenticate metrics server  using  cluster-role.yaml
+2.  RBAC serviceaccount  is configures to authenticate metrics server and k8s apis  using  cluster-role.yaml
 
 3. Request to metrics api can done using below command which is being used in python script to GET pods metrics. 
 
@@ -17,6 +17,12 @@ curl -X GET https://kubernetes.default.svc:443/apis/metrics.k8s.io/v1beta1/pods 
 
 ```
 
+5. Pods status informtion is fetched from k8s api 
+
+```
+https://kubernetes.default.svc:443/api/v1/pods
+
+```
 4.  The monitoring consists of 2 python scripts . One pytohn-app/pyton-app-web-v3.py which periodically(every 2 secs) collects metrics and generates html file at /web/html/index.html. Second one runs a webserver litening on port 8000 of pod which is then exposed as service to access from outside k8s cluster. Both of these scripts are deloyed as containers in pod sharing same volumemount.
   
 
--- python-app/pod-usage-web-v3.py
+++ python-app/pod-usage-web-v3.py
@@ -24,9 +24,7 @@ data3 = data1 + data2
 
 ######## LOAD DATA FROM METRICS SERVER USING CURL COMMAND WITH SERVICE ACCOUNT TOKEN 
 def checkData():
-        data1 = "Authorization: Bearer "
-        data2 = myToken
-        data3 = data1 + data2
+        
 
         response = []
 
@@ -35,7 +33,7 @@ def checkData():
              '-s',
             '-X',
             'GET',
-            'https://kubernetes.docker.internal:6443/apis/metrics.k8s.io/v1beta1/pods',
+            'https://kubernetes.default.svc:443/apis/metrics.k8s.io/v1beta1/pods',
             '--header',
             data3,
             '--insecure',
@@ -44,24 +42,70 @@ def checkData():
 
         return response
 
+### Load Information from kubernetes api server for pod status 
+def checkData2():
+        
+
+        response = []
+
+        response = json.loads(subprocess.check_output([
+            'curl',
+             '-s',
+            '-X',
+            'GET',
+            'https://kubernetes.default.svc:443/api/v1/pods',
+            '--header',
+            data3,
+            '--insecure',
+    
+        ])) 
+
+        return response["items"]
+
 ##### TABULATE index.html output 
 
 run = 1
 it = 0
 while run == 1 :
-
     data = checkData()
-    
-    df = json2html.convert(json = data)
+    data2 = checkData2()
+
     open('/web/html/index.html', 'w').close()
     with open('/web/html/index.html', 'a') as f:
+            sys.stdout = f
+            
+            print('<h1>PYTHON MONITORING</h1>')
+            
+    df = json2html.convert(json = data)
+    
+    with open('/web/html/index.html', 'a') as f:
        sys.stdout = f
-       print('iteration=', it)
+       print("\n",'<h2> POD USAGES :</h2>')
+       print('iteration=', it, "\n")
+       
        print(df)
-                
 
+    time.sleep(1)
+
+    with open('/web/html/index.html', 'a') as f:
+            sys.stdout = f
+            print("\n",'<h2> POD STATUS :</h2>')
+
+
+            for item in data2:
+                df2 = json2html.convert(json = item["status"])
+        
+                phase = json2html.convert(json = item["status"]['phase'])
+                condition = json2html.convert(json = item["status"]['conditions'])
+                status = json2html.convert(json = item["status"]['containerStatuses'])
+
+                
+                print("\n",'<h3> STATUS: ',phase,'</h2>',"\n",condition,"\n",status)
+                print('<br><br>')
+                
+    time.sleep(1)
     it = it + 1        
-    time.sleep(2)
+    
 
 
 
